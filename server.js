52d5d4d249af3e7ddcb464913a0c89113f604a4d
awelch <allenjwelch@gmail.com>
2020-05-03 15:36:49 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ws = require("nodejs-websocket");
+const port = 8080;
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// app.use(routes);
+
+
+// INSERT TESSEL IP ADDRESS HERE. Always prepend with 'ws://' to indicate websocket
+const connection = ws.connect('ws://192.168.128.176:' + port, function() {
+  // When we connect to the server, send some catchy text
+  connection.sendText("Connecting to Tessel...")
+});
+
+// When we get text back
+connection.on('text', function(text) {
+  // print it out
+  console.log("Echoed back from server:", text);
+})
+
+// app.get('/api/hello', (req, res) => {
+//     res.send({ express: 'Hello From Express' });
+// });
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+// }
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+});
